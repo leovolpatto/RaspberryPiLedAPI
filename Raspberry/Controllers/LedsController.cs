2ea165fc44f7d27af30698c1e4bc8d505cbab8aa
leovolpatto <leovolpatto@gmail.com>
2018-12-23 14:17:30 -0200

    first commit

---
+++ Raspberry/Controllers/LedsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RaspberryAPI.Controllers;
+using System.Collections.Generic;
+
+namespace Raspberry.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LedsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<PinStatus>> Get()
+        {
+            return PinStatusMgr.Manager.Pins;
+        }
+        
+        [HttpGet("{id}")]
+        public ActionResult<PinStatus> Get(ushort number)
+        {
+            return PinStatusMgr.Manager.getPinByNumber(number);
+        }
+
+        //http://localhost:49932/api/leds/2/status
+        [HttpPut("{id}/status")]
+        public ActionResult<bool> Put(ushort number, [FromBody] PinStatusChange value)
+        {
+            var pin = PinStatusMgr.Manager.getPinByNumber(number);
+            if(pin == null || value == null)
+            {
+                return false;
+            }
+
+            if (value.ledStatus)
+            {
+                pin.on();
+            }
+            else
+            {
+                pin.off();
+            }
+
+            return true;
+        }
+    }
+
+    public class PinStatusChange
+    {
+        public bool ledStatus { get; set; }
+    }
+}
---
+++ Raspberry/Controllers/PinStatusMgr.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Unosquare.RaspberryIO.Gpio;
+
+namespace RaspberryAPI.Controllers
+{
+    public class PinStatusMgr
+    {
+        public List<PinStatus> Pins { get; private set; }
+
+        private static PinStatusMgr _instance;
+        public static PinStatusMgr Manager
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new PinStatusMgr();
+                }
+
+                return _instance;
+            }
+        }
+
+        public PinStatusMgr()
+        {
+            this.setPins();
+        }
+
+        public PinStatus getPinByNumber(ushort pinNumber)
+        {
+            if(pinNumber > this.Pins.Count)
+            {
+                return null;
+            }
+
+            return this.Pins[pinNumber];
+        }
+
+        private void setPins()
+        {
+            this.Pins = new List<PinStatus>();
+
+            GpioController.Instance.Pins.ToList().ForEach(pin =>
+            {
+                this.Pins.Add(new PinStatus(pin));
+            });
+        }
+    }
+
+    public class PinStatus
+    {
+        public PinStatus(GpioPin pin)
+        {
+            this.Pin = pin;
+            this.Pin.PinMode = GpioPinDriveMode.Output;
+            this.PinNumber = (ushort)pin.PinNumber;
+        }
+
+        public void on()
+        {
+            if (this.IsOn)
+            {
+                return;
+            }
+            
+            this.IsOn = true;
+            this.Pin.Write(true);
+        }
+
+        public void off()
+        {
+            if (!this.IsOn)
+            {
+                return;
+            }
+            
+            this.IsOn = false;
+            this.Pin.Write(false);
+        }
+
+        public ushort PinNumber { get; private set; }
+        public GpioPin Pin { get; private set; }
+        public bool IsOn { get; private set; }
+    }
+}
